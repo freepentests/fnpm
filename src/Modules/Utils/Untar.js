99f938bb257c3e72be80b7e0e6aafe1108d127c8
freepentests <252639873+freepentests@users.noreply.github.com>
2026-05-31 11:47:26 +0100

    did a bit of work on the tar thing

--- src/Modules/Utils/Untar.js
+++ src/Modules/Utils/Untar.js
@@ -3,13 +3,22 @@ import zlib from 'zlib'; // using the zlib library isn't skidding since it's bui
 import fs from 'fs';
 
 export default class Untar {
-	//#untar(tarData) {
-	//}
+	#untar(tarData) {
+		const header = tarData.slice(0, 512);
+		const decoder = new TextDecoder();
+
+		const filename = decoder.decode(header.slice(0, 99));
+		const fileSize = parseInt(decoder.decode(header.slice(124, 135)), 8);
+		const typeFlag = header[156];
+		const checksum = parseInt(decoder.decode(header.slice(148, 155)), 8);
+
+		console.log(filename, fileSize, typeFlag, checksum);
+	}
 
 	extract(archive) {
 		const fileData = fs.readFileSync(archive);
 		const decompressedTarFile = zlib.gunzipSync(fileData); // "gun" zip? more like bomb zip
-		console.log(decompressedTarFile);
+		this.#untar(decompressedTarFile);
 	}
 }
 
